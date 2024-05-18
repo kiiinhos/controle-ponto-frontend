fc4c42638e14499cac665353ba4866eba3af5e66
kiiinhos <viniciuscamargo713@gmail.com>
2024-05-18 17:59:04 -0300

    feat:ajustando service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Link from "next/link";
 import Button from "@/components/Button";
 import UserInput from "@/components/UserInput";
 import Title from "@/components/Title";
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,8 @@
-import axios from "../utils/api";
+import api from "../utils/api";
 
 export const getUserByCode = async (userCode: string) => {
   try {
-    const response = await axios.get(`/check-point/users/${userCode}`);
+    const response = await api.get(`/check-point/users/${userCode}`);
     return response.data;
   } catch (error) {
     throw new Error("Erro ao buscar usuário.");
